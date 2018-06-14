5941b8a373285c685faa52860b0b47bd6f424b1d
nickyreinert <mail@nickyreinert.de>
2018-06-14 14:16:39 +0200

    Update word-cloud.js
    add support for more than one word cloud

--- public/js/word-cloud.js
+++ public/js/word-cloud.js
@@ -10,34 +10,39 @@
 
 	
 	function drawWordCloud(element) {
-
+		
 		var context = element.getContext('2d');
+		
 		var currentWordCloud = $(element).attr('id');
 					
-		wc_wordCloudOptions.color = function (word, weight, fontSize, radius, theta) {
+		var currentWcOptions = window["wc_options_"+currentWordCloud];
+		
+		var currentWcList = window["wc_wordList_"+currentWordCloud];
+		
+		currentWcOptions.color = function (word, weight, fontSize, radius, theta) {
 
 		  var alpha = (weight / 100 );
 		  return "rgba(0,0,0,"+alpha+")";
 
 	  	};
-		wc_wordCloudOptions.hover = window.drawBox;
+		currentWcOptions.hover = window.drawBox;
 	
-		wc_wordCloudOptions.weightFactorFactor = (550 / wc_wordList.length).toFixed(2);
+		currentWcOptions.weightFactorFactor = (550 / currentWcList.length).toFixed(2);
 
-		wc_wordCloudOptions.list = [];
+		currentWcOptions.list = [];
 
-		for (var i = 0; i < wc_wordList.length; i++)
+		for (var i = 0; i < currentWcList.length; i++)
 		{
 			if (i == 0)
 			{
-				var max = parseInt(wc_wordList[i].yValue);
+				var max = parseInt(currentWcList[i].yValue);
 			}
 
-			wc_wordCloudOptions.list[i] = [wc_wordList[i].xValue, parseInt((wc_wordList[i].yValue / max) * 100)];
+			currentWcOptions.list[i] = [currentWcList[i].xValue, parseInt((currentWcList[i].yValue / max) * 100)];
 
 		}
 	
-		WordCloud($('#'+wc_wordCloudOptions['target-id'])[0], wc_wordCloudOptions);
+		WordCloud($('#'+currentWcOptions['target-id'])[0], currentWcOptions);
 
 	}
 	
