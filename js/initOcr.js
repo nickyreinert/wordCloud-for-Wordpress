d3c7557003180c933ddb166e431b364fd322acc5
Tim Krieger <krieger@nlq.nibis.de>
2020-09-17 18:40:46 +0200

    Speeding up text recognition and avoiding issues in mobile browsers

--- js/initOcr.js
+++ js/initOcr.js
@@ -361,7 +361,7 @@
     } else {
 
       workerPath = 'https://unpkg.com/tesseract.js@v2.1.3/dist/worker.min.js';
-      langPath = 'https://tessdata.projectnaptha.com/4.0.0';
+      langPath = 'https://tessdata.projectnaptha.com/4.0.0_fast';
       corePath = 'https://unpkg.com/tesseract.js-core@v2.2.0/tesseract-core.wasm.js';
 
     } 
